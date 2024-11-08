9bb328a8c97e926360b7b2c2b62c37f51595d651
João Antônio A. D. S. S. <joao.antonio.amorim.8@gmail.com>
2024-11-08 00:38:39 -0300

    praticando estilos e configurações
    Configurando um menu de opções

---
+++ caixa-de-opções/logica-001.js
@@ -0,0 +1,10 @@
+/* 
+document.querySelector('p#p-01').addEventListener('mouseover',function(){
+    this.style.borderBottom = '2px solid red';
+    this.style.transitionduration = '2px';
+})
+
+document.querySelector('p#p-01').addEventListener('mouseout',function(){
+    this.style.borderBottom = 'none';
+    this.style.transitionduration = '2px';
+}) */
--- caixa-de-opções/logica.js
+++ caixa-de-opções/logica.js
@@ -1,5 +1,5 @@
 
-    document.querySelector('p#caixa-001').addEventListener('mouseover', function(){
+  /*   document.querySelector('p#caixa-001').addEventListener('mouseover', function(){
         this.style.borderBottom = '2px red solid';
         this.style.transitionduration = '2s';
     })
@@ -7,4 +7,4 @@
     document.querySelector('p#caixa-001').addEventListener('mouseout', function(){
         this.style.borderBottom = '2px black solid';
         this.style.transitionduration = '2s';
-    }) 
+    })  */
